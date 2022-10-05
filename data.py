52b2180ebdb221606d6ae8840766ef23d9432435
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2022-10-05 15:25:13 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,2 +1,5 @@
-
 import pandas as pd
+from cmapPy.pandasGEXpress.parse_gct import parse
+from cmapPy.pandasGEXpress.write_gct import write
+
+data = parse("/Users/pigsnsheep/Downloads/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm\ \(1\).gct")

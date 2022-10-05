a35cb157e7a8d174acaa16955a32a57b36d581a9
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2022-10-05 15:38:04 -0700

    added dataset

--- data.py
+++ data.py
@@ -2,4 +2,6 @@ import pandas as pd
 from cmapPy.pandasGEXpress.parse_gct import parse
 from cmapPy.pandasGEXpress.write_gct import write
 
-data = parse("/Users/pigsnsheep/Downloads/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm\ \(1\).gct")
+data = pd.DataFrame(parse("dataset.gct"))
+
+print("hello world")

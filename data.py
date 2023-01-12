cbb6850d2229be1591e25804347cd8a07d911a43
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2023-01-11 18:02:10 -0800

    Delete data.py

--- data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from cmapPy.pandasGEXpress.parse_gct import parse
-from cmapPy.pandasGEXpress.write_gct import write
-
-data = pd.DataFrame(parse("dataset.gct"))
-
-print("hello world")

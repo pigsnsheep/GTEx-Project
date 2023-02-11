610555eda78e185dc23025c2a6bdf9be2f6c2e15
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2023-02-10 20:36:48 -0800

    Update UMAP.R

--- UMAP.R
+++ UMAP.R
@@ -1,14 +1,14 @@
 #Create a UMAP of the combined data
 combined.umap <- umap(everything)
 
-#Plot each gene using the two dimensions
+#Plot each gene using the two dimensions (please see the Issue for the image)
 combined.umap.data <- data.frame(combined.umap$layout)
 ggplot(combined.umap.data, aes(x=X1,y=X2), fill = X2< -10) + geom_point()
 
-#List of genes in the distinct cluster
+#List of genes in the distinct cluster (Gene_Cluster.csv)
 cluster1 <- combined.umap.data %>% filter(X2 < -10)
 write.csv(cluster1, "Gene_Cluster.csv")
 
-#Visualization of the distinct cluster
+#Visualization of the distinct cluster (please see the Issue for the image)
 combined.umap.data$in.cluster <- combined.umap.data$X2 < -10
 ggplot(combined.umap.data, aes(x=X1,y=X2, colour = in.cluster)) + geom_point()
